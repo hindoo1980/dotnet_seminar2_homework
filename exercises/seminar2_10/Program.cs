35ee011b006aadfb97126c3055701a62203209d8
DENIS <hindoo@mail.ru>
2022-11-06 15:57:17 +0300

    first exercise of lection 1

---
+++ exercises/seminar2_10/Program.cs
@@ -0,0 +1,9 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int FindMiddleDigit (int InputDigit)
+{
+return (InputDigit / 10) % 10;
+}
+Console.WriteLine("Input three-digit number: ");
+int threedigit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"middle digit of {threedigit} is {FindMiddleDigit(threedigit)} ");
